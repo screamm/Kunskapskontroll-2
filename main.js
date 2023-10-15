105de20faee12372012cedfc4a1b1e26e58bae38
David Lindestrand Cuenca <davidrydgren@gmail.com>
2023-10-15 20:53:39 +0200

    Start Assignment 2 - Telephone Book

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+// Lägg till kontakt
+let addContact = document.querySelector("#addContact");
+
+addContact.addEventListener("click", function ()) {
+    let name = document.querySelector("#namn").value;
+    let tele = document.querySelector("#telefon").value;
+    createElement(name, tele);
+    
+}
